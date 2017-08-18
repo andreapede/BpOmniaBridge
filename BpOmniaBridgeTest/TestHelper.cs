3de2274e4adf6cea02a92177277e30b20d36b562
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2017-08-18 16:00:38 +1000

    TestHelper added to avoid reuse safe function over the testing code

---
+++ BpOmniaBridgeTest/TestHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BpOmniaBridgeTest
+{
+    class TestHelper
+    {
+        public string CopyFileToTest(string fileName)
+        {
+            var currentFolder = Directory.GetCurrentDirectory().Replace("\\bin\\Debug", "");
+            string fileToMove = Path.Combine(currentFolder, "toTest", fileName + ".out");
+            var cmnDocPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
+            var destFileName = Path.Combine(cmnDocPath, "BpOmniaBridge", "temp_files", fileName + ".out");
+
+            File.Copy(fileToMove, destFileName, true);
+
+            return destFileName;
+        }
+
+        public void DeleteTempFileIN(string fileName)
+        {
+            var cmnDocPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
+            var filePath = Path.Combine(cmnDocPath, "BpOmniaBridge", "temp_files", fileName + ".in");
+            File.Delete(filePath);
+        }
+    }
+}
