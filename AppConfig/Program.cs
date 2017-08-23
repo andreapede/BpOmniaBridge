139cf818488700865798578a6c7095c5ffd59a2e
Emanuele Magliozzi <emanuele.magliozzi@gmail.com>
2017-08-23 17:06:36 +1000

    Console app added for the app configuration - Omnia settings in ACS done - need to go ahead with BP regestry

---
+++ AppConfig/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Xml.Linq;
+
+namespace AppConfig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BpOmniaBridge is setting up Omnia:");
+            
+            var cmnDocPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonDocuments);
+            var acsFile = Path.Combine(cmnDocPath, "Cosmed", "CosmedLab", "acs.xml");
+            var bridgeTempFile = Path.Combine(cmnDocPath, "BpOmniaBridge", "temp_files");
+            XDocument doc = XDocument.Load(acsFile);
+            XElement bridgeControl = doc.Element("ExtendedConfiguration").Element("Bridge").Element("Control");
+            bridgeControl.Attribute("ID").Value = "file";
+            Console.WriteLine("... ID -> file ...");
+            Thread.Sleep(500);
+            bridgeControl.Attribute("Value").Value = "xml";
+            Console.WriteLine("... Value -> xml ...");
+            Thread.Sleep(500);
+            bridgeControl.Attribute("Item2").Value = "utf-8";
+            Console.WriteLine("... Item2 -> utf-8 ...");
+            Thread.Sleep(500);
+            bridgeControl.Attribute("Item3").Value = bridgeTempFile;
+            Console.WriteLine("... Item3 -> " + bridgeTempFile + " ...");
+            Thread.Sleep(500);
+            bridgeControl.Attribute("Item4").Value = "OmniaXB";
+            Console.WriteLine("... Item4 -> OmniaXB ...");
+            Thread.Sleep(500);
+
+            doc.Save(acsFile);
+        
+            Console.WriteLine("Omnia settings updated - please create User as per documentation");
+            Console.WriteLine(" ");
+            Console.WriteLine("....please wait....");
+            Console.WriteLine(" ");
+            Thread.Sleep(2000);
+        }
+    }
+}
